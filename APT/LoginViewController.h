d833bad7b0d48bd07813b9a3362c9ee4d51f59b5
latieshacaston <aciddrops23@gmail.com>
2015-11-20 00:36:30 -0500

    updated sign in page

--- APT/LoginViewController.h
+++ APT/LoginViewController.h
@@ -9,6 +9,15 @@
 @interface LoginViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *logInButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *signUpButton;
+
+
+
+
 - (IBAction)login:(id)sender;
 
 @end
